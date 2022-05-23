db0e92acee96bcdda54f4524dcba7e3e57a5eda9
EstebanPelaez <87784235+EstebanPelaez@users.noreply.github.com>
2022-05-23 09:58:58 -0500

    Version 1.0.3 Se han implementado las pruebas unitarias de las funcionalidades de la aplicación web.

--- persistencia/src/test/java/co/edu/uniquindio/CinecoOnly/CinecoOnly/PeliculaTest.java
+++ persistencia/src/test/java/co/edu/uniquindio/CinecoOnly/CinecoOnly/PeliculaTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -26,9 +27,9 @@ public class PeliculaTest {
     @Sql("classpath:peliculaPrueba.sql")
     public void registrarTest(){
 
-        LocalDate fecha = LocalDate.of(2022, 5 , 15);
-        LocalDate fechainico = LocalDate.of(2021, 5 , 15);
-        Pelicula pelicula = new Pelicula(33, "Es demasiado buena", fecha, fechainico, null, "Shrk 2");
+        LocalDate fecha = LocalDate.of(2023, 5 , 15);
+        LocalDate fechainico = LocalDate.of(2022, 5 , 25);
+        Pelicula pelicula = new Pelicula(33, "Shrek 3", fechainico, fecha, null, "De las mejores peliculas");
         peliculaRepo.save(pelicula);
 
         Assertions.assertNotNull(peliculaRepo.findById(66));
@@ -42,12 +43,12 @@ public class PeliculaTest {
     @Sql("classpath:peliculaPrueba.sql")
     public void actualizarTest(){
 
-        Pelicula pelicula = peliculaRepo.findById(0).orElse(null);
+        Pelicula pelicula = peliculaRepo.findById(30).orElse(null);
 
         pelicula.setNombre("X-men");
         peliculaRepo.save(pelicula);
 
-        Pelicula pelicualnew = peliculaRepo.findById(0).orElse(null);
+        Pelicula pelicualnew = peliculaRepo.findById(30).orElse(null);
         Assertions.assertEquals("X-men", pelicualnew.getNombre());
     }
 
@@ -59,9 +60,50 @@ public class PeliculaTest {
     @Sql("classpath:peliculaPrueba.sql")
     public void eliminarTest(){
 
-        peliculaRepo.deleteById(0);
+        peliculaRepo.deleteById(29);
         Pelicula peliculaNew = peliculaRepo.findById(0).orElse(null);
 
         Assertions.assertNull(peliculaNew);
     }
+
+    /**
+     * En este método se listan las peliculas que esta en el cartelera en peliculaPrueba.sql, con el fin de realizar
+     * una prueba unitaria
+     */
+    @Test
+    @Sql("classpath:peliculaPrueba.sql")
+    public void listarPeliculas(){
+
+        List<Pelicula> peliculas = peliculaRepo.listarCartelera();
+        int contador =0;
+        for(Pelicula miPeli : peliculas){
+            contador = +1;
+            if (peliculas.size()==contador){
+                System.out.println(miPeli.getNombre());
+            }else{
+                System.out.println("No concuerda la cantidad de peliculas");
+            }
+        }
+    }
+
+    /**
+     * En este método se listan las peliculas que esta en el cartelera en peliculaPrueba.sql, con el fin de realizar
+     * una prueba unitaria
+     */
+    @Test
+    @Sql("classpath:peliculaPrueba.sql")
+    public void listarPeliculasProximas(){
+
+        List<Pelicula> peliculas = peliculaRepo.listarProximosEstrenos();
+        int contador =0;
+        for(Pelicula miPeli : peliculas){
+            contador = +1;
+            if (peliculas.size()==contador){
+                System.out.println(miPeli.getNombre());
+            }else{
+                System.out.println("No concuerda la cantidad de peliculas");
+            }
+        }
+    }
+
 }
--- persistencia/src/test/java/co/edu/uniquindio/CinecoOnly/CinecoOnly/TicketTest.java
+++ persistencia/src/test/java/co/edu/uniquindio/CinecoOnly/CinecoOnly/TicketTest.java
@@ -1,11 +1,15 @@
 package co.edu.uniquindio.CinecoOnly.CinecoOnly;
 
-import co.edu.uniquindio.CinecoOnly.CinecoOnly.repositorios.TicketRepo;
+import co.edu.uniquindio.CinecoOnly.CinecoOnly.entidades.*;
+import co.edu.uniquindio.CinecoOnly.CinecoOnly.repositorios.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.time.LocalDate;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -13,6 +17,18 @@ public class TicketTest {
 
     @Autowired
     private TicketRepo ticketRepo;
+
+    @Autowired
+    private AsientoRepo asientoRepo;
+
+    @Autowired
+    private Tipo_TicketRepo tipo_ticketRepo;
+
+    @Autowired
+    private ClienteRepo clienteRepo;
+
+    @Autowired
+    private FuncionRepo funcionRepo;
 
     /**
      * En este método se crea un ticket, con el fin de realizar una prueba unitaria
@@ -22,5 +38,45 @@ public class TicketTest {
     @Sql("classpath:ticketPrueba.sql")
     public void registrarTest(){
 
+        LocalDate fecha = LocalDate.of(2023, 5 , 15);
+        Cliente cliente = clienteRepo.getById(1);
+        Funcion funcion = funcionRepo.getById(1);
+        Tipo_Ticket tipoTicket = tipo_ticketRepo.getById(1);
+        Asiento asiento = asientoRepo.getById(1);
+
+        Ticket ticket = new Ticket(2, fecha, "Disponible", tipoTicket, asiento, cliente, funcion);
+        ticketRepo.save(ticket);
+
+        Assertions.assertNotNull(ticketRepo.findById(2));
+    }
+
+    /**
+     * En este método se actualiza el nombre de una sala que está en el repositorio clientePrueba.sql, con el fin
+     * de realizar una prueba unitaria
+     */
+    @Test
+    @Sql("classpath:ticketPrueba.sql")
+    public void actualizarTest(){
+
+        Ticket ticketNew = ticketRepo.findById(1).orElse(null);
+        ticketNew.setEstado("Ocupado");
+        ticketRepo.save(ticketNew);
+
+        Ticket ticketNuevo = ticketRepo.findById(1).orElse(null);
+        Assertions.assertEquals("Ocupado", ticketNuevo.getEstado());
+    }
+
+    /**
+     * En este método se elimina una sala que esta en el repositorio salaPrueba.sql, con el fin de realizar
+     * una prueba unitaria
+     */
+    @Test
+    @Sql("classpath:ticketPrueba.sql")
+    public void eliminarTest(){
+
+        ticketRepo.deleteById(1);
+        Ticket ticketBuscado = ticketRepo.findById(1).orElse(null);
+
+        Assertions.assertNull(ticketBuscado);
     }
 }
